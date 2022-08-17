034ea55cc4f93127be29a4cb0fb826ca8ae87175
Corbin <corbin.featherstone@gmail.com>
2022-08-17 22:32:18 +1200

    Liquid template starting structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./_site/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: '#FFCA2E',
+          dark: '#F7BC28',
+          light: '#FFCE55',
+        },
+      },
+    },
   },
   plugins: [],
 }
