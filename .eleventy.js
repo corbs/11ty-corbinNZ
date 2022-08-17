b46d75505b8445458c79fd99b9c3780cc33cdf77
Corbin <corbin.featherstone@gmail.com>
2022-08-18 00:40:09 +1200

    Homepage collections loop recent work

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
-    // Input Directory : src Output directory: _site
 
+    // Input Directory : src Output directory: _site
     return {
         dir: { input: 'src', output: '_site' }
     }; 
