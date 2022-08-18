4111cf7a9ea4529ec6a7023e2f011ad597d60a78
Corbin <corbin.featherstone@gmail.com>
2022-08-19 00:08:32 +1200

    New featured collections script

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,56 @@
 module.exports = function(eleventyConfig) {
 
+    // Create collections group featuredWebsite 
+    eleventyConfig.addCollection("featuredWebsite", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "website");
+      });
+
+    // Create collections group featuredBebsite 
+    eleventyConfig.addCollection("featuredBranding", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "branding");
+      });
+
+    // Create collections group featuredLayout 
+    eleventyConfig.addCollection("featuredLayout", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "layout");
+      });
+
+    // Create collections group featuredAdvertising
+    eleventyConfig.addCollection("featuredAdvertising", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "advertising");
+      });
+
+    // Create collections group featuredVideo
+    eleventyConfig.addCollection("featuredVideo", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "video");
+      });
+
+    // Create collections group featuredMotionGraphics
+    eleventyConfig.addCollection("featuredMotionGraphics", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "motion graphics");
+      });
+
+    // Create collections group featuredPackaging
+    eleventyConfig.addCollection("featuredPackaging", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "packaging");
+      });
+
+    // Create collections group featuredIllustration
+    eleventyConfig.addCollection("featuredIllustration", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "illustration");
+      });
+
+    // Create collections group featuredEvents
+    eleventyConfig.addCollection("featuredEvents", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "events");
+      });
+
+    // Create collections group featuredArtwork
+    eleventyConfig.addCollection("featuredArtwork", function (collectionApi) {
+        return collectionApi.getFilteredByTags("featured", "artwork");
+      });
+
+
     // Input Directory : src Output directory: _site
     return {
         dir: { input: 'src', output: '_site' }
