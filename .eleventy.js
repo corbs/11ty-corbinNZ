c19da3154233cab2c86dcb749401faa60886ce14
Corbin <corbin.featherstone@gmail.com>
2022-08-25 22:32:03 +1200

    Work layout styles scaffold

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
 module.exports = function(eleventyConfig) {
 
-  // Limit filter - usage: {{ for item in collections.all | limit(3) }}
+  // Limit filter - usage: {% for item in collections.all | limit(3) %}
     eleventyConfig.addFilter("limit", function (arr, limit) {
       return arr.slice(0, limit);
+    });
+
+    // Random limit filter exlcuding current page - usage: {% for item in collections.all | randomLimit(3, page.url) %} 
+    eleventyConfig.addFilter("randomLimit", (arr, limit, currPage) => {
+      // Filters out current page
+      const pageArr = arr.filter((page) => page.url !== currPage);
+      // Randomizes remaining items
+      pageArr.sort(() => {
+        return 0.5 - Math.random();
+      });
+      // Returns array items up to limit
+      return pageArr.slice(0, limit);
     });
 
     // Create collections group featuredWebsite 
