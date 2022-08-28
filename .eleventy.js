ffef0a1065ae29f1fc9f64f165342ab3a6632aa0
Corbin <corbin.featherstone@gmail.com>
2022-08-28 13:30:51 +1200

    Imgix upgrade to personal plus gcloud

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,5 @@
 module.exports = function(eleventyConfig) {
 
-  // Limit filter - usage: {% for item in collections.all | limit(3) %}
-    eleventyConfig.addFilter("limit", function (arr, limit) {
-      return arr.slice(0, limit);
-    });
-
     // Random limit filter exlcuding current page - usage: {% for item in collections.all | randomLimit(3, page.url) %} 
     eleventyConfig.addFilter("randomLimit", (arr, limit, currPage) => {
       // Filters out current page
@@ -18,56 +13,11 @@ module.exports = function(eleventyConfig) {
     });
 
     // Create collections group featuredWebsite 
-    eleventyConfig.addCollection("featuredWebsite", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "website");
-      });
+    // eleventyConfig.addCollection("featuredWebsite", function (collectionApi) {
+    //    return collectionApi.getFilteredByTags("featured", "website");
+    //  });
 
-    // Create collections group featuredBebsite 
-    eleventyConfig.addCollection("featuredBranding", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "branding");
-      });
-
-    // Create collections group featuredLayout 
-    eleventyConfig.addCollection("featuredLayout", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "layout");
-      });
-
-    // Create collections group featuredAdvertising
-    eleventyConfig.addCollection("featuredAdvertising", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "advertising");
-      });
-
-    // Create collections group featuredVideo
-    eleventyConfig.addCollection("featuredVideo", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "video");
-      });
-
-    // Create collections group featuredMotionGraphics
-    eleventyConfig.addCollection("featuredMotionGraphics", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "motion graphics");
-      });
-
-    // Create collections group featuredPackaging
-    eleventyConfig.addCollection("featuredPackaging", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "packaging");
-      });
-
-    // Create collections group featuredIllustration
-    eleventyConfig.addCollection("featuredIllustration", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "illustration");
-      });
-
-    // Create collections group featuredEvents
-    eleventyConfig.addCollection("featuredEvents", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "events");
-      });
-
-    // Create collections group featuredArtwork
-    eleventyConfig.addCollection("featuredArtwork", function (collectionApi) {
-        return collectionApi.getFilteredByTags("featured", "artwork");
-      });
-
-
+    
     // Input Directory : src Output directory: _site
     return {
         dir: { input: 'src', output: '_site' }
